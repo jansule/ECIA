5a99b797fb7c2a8a265ddecc4028b0f87417e56b
Lasse Einfeldt <Lasse Einfeldt>
2016-05-26 14:49:04 +0200

    add leap file

---
+++ public/js/leap.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var element = document.getElementById('text');
+var dir =  document.getElementById("dir");
+element.style.color = "orange";
+
+var color = ["red", "green", "blue", "orange"];
+var i = 0;
+
+Leap.loop({enableGestures: true}, function(frame){
+    if(frame.valid && frame.gestures.length > 0){
+        frame.gestures.forEach(function(gesture){
+            switch (gesture.type){
+                case "circle":
+                    //element.style.color = color[i];
+                    if(i<4){i++} else {i=0}
+                    element.innerHTML = "Sie haben einen Kreis gezeichnet.";
+                    break;
+                case "swipe":
+                    element.style.color = color[i];
+                    if(i<4){i++} else {i=0}
+                    element.innerHTML = "Sie haben eine Wischgeste vollführt!";
+                    break;
+            }
+        });
+    }
+});
+
+
+Leap.loop({enablesGestures: true}, function frame(frame) {
+    frame.hands.forEach(function(hand, index) {
+       /*if(hand.direction[0] > 0) {
+         element.innerHTML = "Swipe to the right"; 
+        }
+        if(hand.direction[0] < 0) {
+           element.innerHTML = "Swipe to the left";
+        }
+        })*/
+        dir.innerHTML = hand.direction;
+    })
+})
