0a53a815d73df9a48f4a984a070a3b24e88a9a02
slim01 <s.slimani@gmx.de>
2016-05-31 22:26:16 +0200

    connect finger control to slides

--- public/js/leap.js
+++ public/js/leap.js
@@ -3,27 +3,29 @@
 var element = document.getElementById('text');
 var dir =  document.getElementById("dir");
 element.style.color = "red";
-
-//var color = ["red", "green", "blue", "orange"];
-var i = 0;
-
 var waitMode = false; 
 
 Leap.loop({enableGestures: true}, function(frame){
     if(!waitMode && frame.valid && frame.hands.length > 0){
-				
-			var numberOfFingers = getExtendedFingers(frame.hands[0]);
-			element.innerHTML = "You've shown " + numberOfFingers + " fingers.";	
-			waitMode = true; 
-			setTimeout(function(){ waitMode = false; }, 900); // need timeout for not recognising fingers again directly
+		waitMode = true; 		
+		var numberOfFingers = getExtendedFingers(frame.hands[0]);
+		element.innerHTML = "You've shown " + numberOfFingers + " fingers.";	
+		console.log("fingers: " + numberOfFingers);
+		
+		var destination = numberOfFingers-1;
+		if(destination > -1){
+			$.fn.fullpage.silentMoveTo(0, destination);	
+		}
+		
+		setTimeout(function(){ waitMode = false; }, 1000); // need timeout for not recognising fingers again directly
 			
     }
 });
 
 function getExtendedFingers(hand){
    var exfingers = 0;
-   for(var i=0;i<hand.fingers.length;i++){
-      if(hand.fingers[i].extended){
+   for(var j=0;j<hand.fingers.length;j++){
+      if(hand.fingers[j].extended){
          exfingers++;
       }
    }
