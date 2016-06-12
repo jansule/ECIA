5bd5fd55706f6228af3025012e0ab27ad04688e9
slim01 <s.slimani@gmx.de>
2016-06-12 20:34:41 +0200

    gestures for audio and visibility on/off

--- public/js/leap.js
+++ public/js/leap.js
@@ -4,6 +4,7 @@ var element = document.getElementById('text');
 var dir =  document.getElementById("dir");
 element.style.color = "red";
 var waitMode = false; 
+var oldFrame = false;
 
 $( document ).ready(function() {
 	
@@ -15,13 +16,32 @@ $( document ).ready(function() {
 			console.log("fingers: " + numberOfFingers);
 			
 			var destination = numberOfFingers-1;
-			if(destination > -1 && destination < 5){
+			if(destination > -1 && destination < 4){
 				$.fn.fullpage.silentMoveTo(0, destination);	
+			}
+
+		    //if 5 fingers are shown the application should be turned on/off
+			if(destination == 4){
+				
+				//TODO: turn visibility of iframe on/off
 			}
 			
 			setTimeout(function(){ waitMode = false; }, 1000); // need timeout for not recognising fingers again directly
 				
 		}
+		if(!oldFrame){
+		  oldFrame = frame;
+		}
+
+		if(frame.valid && frame.gestures[0] && !oldFrame.gestures[0]){
+		  frame.gestures.forEach(function(gesture){
+			  if(gesture.type == "circle"){
+				   //TODO: turn on audio control
+				   console.log("Circle Gesture");                 
+			  }
+          });
+        }
+		oldFrame = frame;
 	});
 
 	function getExtendedFingers(hand){
