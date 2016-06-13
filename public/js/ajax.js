95fad0bd54069551ea6ccfaaf5e02e0d4e9e4310
jansule <j_sule02@wwu.de>
2016-06-13 23:53:57 +0200

    add new tracks and statistics

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -8,89 +8,83 @@ $(document).ready(function(){
 	var currentTrack;
 	var ownTracks = [];
 	var sameCar = [];
-	$.ajax({
-		type: 'get',
-		url: 'https://envirocar.org/api/stable/tracks',
-		success: function(xhr, textStatus, data){
-			var _parsedResponse = JSON.parse(data.responseText);
-			ids = _parsedResponse.tracks;
 
+		$.ajax({
+			type: 'get',
+			url: 'https://envirocar.org/api/stable/tracks/575e5184e4b0a69192ac65d1',
+			success: function(xhr, textStatus, data){
+				currentTrack = JSON.parse(data.responseText);
+				console.log(currentTrack);
 
+				$.ajax({
+					type: 'get',
+					url: 'https://envirocar.org/api/stable/tracks',
+					success: function(xhr, textStatus, data){
+						var _parsedResponse = JSON.parse(data.responseText);
+						ids = _parsedResponse.tracks;
 
+								for(var index in ids){
 
-			$.ajax({
-				type: 'get',
-				url: 'https://envirocar.org/api/stable/statistics/',
-				success: function(xhr, textStatus, data){
-					stats = JSON.parse(data.responseText);
-					for(index in ids){
+									$.ajax({
+										type: 'get',
+										url: 'https://envirocar.org/api/stable/tracks/' + ids[index].id,
+										success: function(xhr, textStatus, data){
+											var _parsedTrack = JSON.parse(data.responseText);
+											tracks.push(_parsedTrack);
+											if(_parsedTrack.properties.sensor.properties.model == currentTrack.properties.sensor.properties.model){
+												sameCar.push(_parsedTrack);
+											}
 
-
-
-						$.ajax({
-							type: 'get',
-							url: 'https://envirocar.org/api/stable/tracks/' + ids[index].id,
-							success: function(xhr, textStatus, data){
-								var _parsedTrack = JSON.parse(data.responseText);
-								tracks.push(_parsedTrack);
-								if((index != 0) && (_parsedTrack.properties.sensor.properties.model == tracks[0].properties.sensor.properties.model)){
-									sameCar.push(_parsedTrack);
+										},
+										error: function(textStatus, errorThrown){
+											console.log(errorThrown);
+										}
+									});
 								}
-
-							},
-							error: function(textStatus, errorThrown){
-								console.log(errorThrown);
-							}
-						});
-					}
-
-					setTimeout(function(){
-						currentTrack = tracks[0];
-						for(var i=1; i<=10; i++){
-							ownTracks.push(tracks[i]);
-						}
-						tracks.splice(0, 11);
-						$('#stats').text(JSON.stringify(stats));
-						$('#current').text(JSON.stringify(currentTrack));
-						$('#sameCar').text(JSON.stringify(sameCar));
-						$('#own').text(JSON.stringify(ownTracks));
-
-
-						for(index in currentTrack.features){
-							(function(ind){
+								console.log(sameCar);
 								setTimeout(function(){
-
-									/*
-									*
-									*
-									##############################################################
-									##############################################################
-									#### Put here code for calculating live stats and display ####
-									##############################################################
-									##############################################################
-									*
-									*/
-									//console.log('Coordinates: ' + currentTrack.features[ind].geometry.coordinates[0] + ' ' + currentTrack.features[ind].geometry.coordinates[1]);
-								}, 5000 * ind);
-							})(index);
-						}
-					}, 5000);
-						
-						
-
-				},
-				error: function(textStatus, errorThrown){
-					console.log(errorThrown);
-				}
-			});
+									for(var i=1; i<=10; i++){
+										ownTracks.push(tracks[i]);
+									}
+									tracks.splice(0, 11);
+									$('#current').text(JSON.stringify(currentTrack));
+									$('#sameCar').text(JSON.stringify(sameCar));
+									$('#own').text(JSON.stringify(ownTracks));
 
 
-		},
-		error: function(textStatus, errorThrown){
-			console.log(errorThrown);	
-		}
-	});
+									for(index in currentTrack.features){
+										(function(ind){
+											setTimeout(function(){
 
+												/*
+												*
+												*
+												##############################################################
+												##############################################################
+												#### Put here code for calculating live stats and display ####
+												##############################################################
+												##############################################################
+												*
+												*/
+												console.log('Coordinates: ' + currentTrack.features[ind].geometry.coordinates[0] + ' ' + currentTrack.features[ind].geometry.coordinates[1]);
+											}, 5000 * ind);
+										})(index);
+									}
+								}, 5000);
+
+
+					},
+					error: function(textStatus, errorThrown){
+						console.log(errorThrown);	
+					}
+				});
+
+			},
+			error: function(textStatus, errorThrown){
+				console.log(errorThrown);
+			}
+		});		
+				
 
 	
 
