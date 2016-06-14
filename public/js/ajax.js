d8f6432962979fe3cd4301c1778275081e2dddb0
jansule <j_sule02@wwu.de>
2016-06-14 14:50:08 +0200

    update statsCalculation

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -8,77 +8,82 @@ $(document).ready(function(){
 	var currentTrack;
 	var ownTracks = [];
 	var sameCar = [];
+	var sameCarStats;
 
 		$.ajax({
 			type: 'get',
 			url: 'https://envirocar.org/api/stable/tracks/575e5184e4b0a69192ac65d1',
 			success: function(xhr, textStatus, data){
 				currentTrack = JSON.parse(data.responseText);
-				console.log(currentTrack);
 
 				$.ajax({
 					type: 'get',
-					url: 'https://envirocar.org/api/stable/tracks',
+					url: 'https://envirocar.org/api/stable/sensors/' + currentTrack.properties.sensor.properties.id + '/statistics',
 					success: function(xhr, textStatus, data){
-						var _parsedResponse = JSON.parse(data.responseText);
-						ids = _parsedResponse.tracks;
+						sameCarStats = JSON.parse(data.responseText);
+						$.ajax({
+						type: 'get',
+						url: 'https://envirocar.org/api/stable/tracks',
+						success: function(xhr, textStatus, data){
+							var _parsedResponse = JSON.parse(data.responseText);
+							ids = _parsedResponse.tracks;
 
-								for(var index in ids){
+									for(var index in ids){
+										
 
-									$.ajax({
-										type: 'get',
-										url: 'https://envirocar.org/api/stable/tracks/' + ids[index].id,
-										success: function(xhr, textStatus, data){
-											var _parsedTrack = JSON.parse(data.responseText);
-											tracks.push(_parsedTrack);
-											if(_parsedTrack.properties.sensor.properties.model == currentTrack.properties.sensor.properties.model){
-												sameCar.push(_parsedTrack);
+										$.ajax({
+											type: 'get',
+											url: 'https://envirocar.org/api/stable/tracks/' + ids[index].id,
+											success: function(xhr, textStatus, data){
+												var _parsedTrack = JSON.parse(data.responseText);
+												if(_parsedTrack.properties.sensor.properties.model == currentTrack.properties.sensor.properties.model){
+													$.ajax({
+														type: 'get',
+														url: 'https://envirocar.org/api/stable/tracks/' +_parsedTrack.properties.id+ '/statistics',
+														success: function(xhr, textStatus, data){
+															sameCar.push(JSON.parse(data.responseText));
+														},
+														error: function(textStatus, errorThrown){
+															console.log(errorThrown);
+														}
+													})
+												}
+
+											},
+											error: function(textStatus, errorThrown){
+												console.log(errorThrown);
 											}
-
-										},
-										error: function(textStatus, errorThrown){
-											console.log(errorThrown);
-										}
-									});
-								}
-								console.log(sameCar);
-								setTimeout(function(){
-									for(var i=1; i<=10; i++){
-										ownTracks.push(tracks[i]);
+										});
+										
 									}
-									tracks.splice(0, 11);
-									$('#current').text(JSON.stringify(currentTrack));
-									$('#sameCar').text(JSON.stringify(sameCar));
-									$('#own').text(JSON.stringify(ownTracks));
+									setTimeout(function(){
+										console.log(sameCar);
+										console.log(sameCar.length);
+										sameCar = sameCar.slice(0,3);
+										console.log(sameCar.length);
+
+										$('#current').text(JSON.stringify(currentTrack));
+										$('#sameCar').text(JSON.stringify(sameCar));
+										$('#own').text(JSON.stringify(sameCarStats));
+									}, 5000);
+										
 
 
-									for(index in currentTrack.features){
-										(function(ind){
-											setTimeout(function(){
+										
 
-												/*
-												*
-												*
-												##############################################################
-												##############################################################
-												#### Put here code for calculating live stats and display ####
-												##############################################################
-												##############################################################
-												*
-												*/
-												console.log('Coordinates: ' + currentTrack.features[ind].geometry.coordinates[0] + ' ' + currentTrack.features[ind].geometry.coordinates[1]);
-											}, 5000 * ind);
-										})(index);
-									}
-								}, 5000);
 
+						},
+						error: function(textStatus, errorThrown){
+							console.log(errorThrown);	
+						}
+					});
 
 					},
 					error: function(textStatus, errorThrown){
-						console.log(errorThrown);	
+						console.log(errorThrown);
 					}
 				});
-
+				
 			},
 			error: function(textStatus, errorThrown){
 				console.log(errorThrown);
