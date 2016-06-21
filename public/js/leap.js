a167e668c4fcee766278cef626a3884ef1d6ba28
slim01 <s.slimani@gmx.de>
2016-06-21 15:54:50 +0200

    add leapGestures Plugin, audio control via fist
    I changed our code to a leap-gestures plugin with MIT-license. Works
    slightly better then our implementation. The audio output is activated
    via a fist-gestures which causes much less misinterpretation.

--- public/js/leap.js
+++ public/js/leap.js
@@ -1,69 +1,28 @@
 'use strict';
 
-var element = document.getElementById('text');
-var dir = document.getElementById("dir");
-//element.style.color = "red";
-var waitMode = false;
-var oldFrame = false;
+function hand(e) {
+    if (e.fingers == 0) {
+        console.log("fist detected");
 
-$(document).ready(function() {
+        //active audioOutput on fist detected
+        var currentIndex = $('.slide.active').index();
+        audioOutput(currentIndex+1);
+    } else {
+        console.log(e.fingers + "fingers detected");
 
-    Leap.loop({
-        enableGestures: true
-    }, function(frame) {
-        if (!waitMode && frame.valid && frame.hands.length > 0) {
-            waitMode = true;
-            var numberOfFingers = getExtendedFingers(frame.hands[0]);
-            //element.innerHTML = "You've shown " + numberOfFingers + " fingers.";
-            console.log("fingers: " + numberOfFingers);
-
-            var destination = numberOfFingers - 1;
-            if (destination > -1 && destination < 5) {
-                $.fn.fullpage.silentMoveTo(0, destination);
-            }
-
-           
-
-            setTimeout(function() {
-                waitMode = false;
-            }, 1000); // need timeout for not recognising fingers again directly
-
+        //change slide
+        var destination = e.fingers - 1;
+        if (destination > -1 && destination < 5) {
+            $.fn.fullpage.silentMoveTo(0, destination);
         }
-        //check if frame is already seen to recognise the gestures discrete
-        if (!oldFrame) {
-            oldFrame = frame;
-        }
-
-        if (frame.valid && frame.gestures[0] && !oldFrame.gestures[0]) {
-            frame.gestures.forEach(function(gesture) {
-                if (gesture.type == "circle") {
-                    var currentIndex = $('.slide.active').index();
-                    console.log(currentIndex);
-                    audioOutput(currentIndex);
-                    console.log("Circle Gesture");
-                }
-            });
-        }
-        oldFrame = frame;
-    });
-
-    function getExtendedFingers(hand) {
-        var exfingers = 0;
-        for (var j = 0; j < hand.fingers.length; j++) {
-            if (hand.fingers[j].extended) {
-                exfingers++;
-            }
-        }
-        return exfingers;
     }
+}
 
-
-    Leap.loop({
-        enablesGestures: true
-    }, function frame(frame) {
-        frame.hands.forEach(function(hand, index) {
-           
-            //dir.innerHTML = hand.direction;
-        })
-    })
-});
+var config = {
+    useControllerEvents: true,
+    confidenceThreshold: 0.15,
+    handDuration: 1500
+}
+var leapGestures = new LeapGestures(config);
+var c = leapGestures.getLeapController();
+c.on("gestureHand", hand);
