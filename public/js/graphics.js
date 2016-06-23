68bf316a95994925f9e26c7f1c08510220a396ee
jansule <j_sule02@wwu.de>
2016-06-23 17:19:10 +0200

    add new charts

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -23,228 +23,149 @@ for(var index in enviro.currentTrack.features){
 			/**
 			 * First page
  			 */
+ 			 
 			FP_speed.innerHTML = Math.floor(liveData[ind].properties.phenomenons.Speed.value * 100) / 100 + " km/h";
 			FP_cons.innerHTML = Math.floor(liveData[ind].properties.phenomenons.Consumption.value * 100) / 100 + " l/h";
 			FP_CO2.innerHTML = Math.floor(liveData[ind].properties.phenomenons.CO2.value * 100) / 100 + " kg/h";
 
 			
-			/**
-			 * Second page
-             */
-			document.getElementById('SP_speed').innerHTML = "";
-			SP_speed = new Rickshaw.Graph( {
-				element: document.getElementById("SP_speed"),
-				width: 450,
-				height: 150,
-				renderer: 'multi',
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.Speed.value} ],
-						color: '#0072ff',
-						name: 'current',
-						renderer: 'bar'
-					}, {
-						data: [ { x: 0, y: enviro.ownTracksStats.statistics[0].avg}, {x: 1, y:enviro.ownTracksStats.statistics[0].avg} ],
-						color: 'red',
-						name: 'average',
-						renderer: 'line'
+		/*################
+		####Second slide##
+		##################*/
+		var sp_speed_options = {
+			xaxis: {
+				max: 1,
+				ticks: 0
+			},
+			yaxis: {
+				max: enviro.ownTracksStats.statistics[2].max,
+				ticks: 0
+			}
+		};
+		var sp_speed = $.plot('#SP_speed', [{
+			data: [[0, liveData[ind].properties.phenomenons.Speed.value]],
+			bars: {
+				show: true,
+				fill: 1,
+				fillColor: 'steelblue',
+				lineWidth: 0
+			},
+			color: 'steelblue',
+			shadowSize: 0
+		},{
+			data: [[0, calcLiveAverage(liveData.slice(0, ind + 1)).avgSpeed], [1, calcLiveAverage(liveData.slice(0, ind + 1)).avgSpeed]],
+			lines: {
+				show: true,
+				lineWidth: 3
+			},
+			color: '#FF0000',
+			shadowSize: 4
+		}], sp_speed_options);
 
-				} ]
-			} );
-			//SP_speed_y = new Rickshaw.Graph.Axis.Y({graph: SP_speed})
-			//SP_speed_y.render();
-			SP_speed.render();
-			
-			document.getElementById('SP_cons').innerHTML = "";
-			SP_cons = new Rickshaw.Graph( {
-				element: document.getElementById("SP_cons"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.Consumption.value} ],
-						color: '#0072ff',
-						name: 'current'
-					}, {
-						data: [ { x: 0, y: enviro.ownTracksStats.statistics[2].avg} ],
-						color: '#708090',
-						name: 'average'
+		var o = sp_speed.pointOffset({ x: 1.2, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgSpeed});
 
-				} ]
-			} );
-			SP_cons_y = new Rickshaw.Graph.Axis.Y({graph: SP_cons})
-			SP_cons_y.render();
-			SP_cons.render();
+		// Append it to the placeholder that Flot already uses for positioning
 
-			document.getElementById('SP_co2').innerHTML = "";
-			SP_co2 = new Rickshaw.Graph( {
-				element: document.getElementById("SP_co2"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.CO2.value} ],
-						color: '#0072ff',
-						name: 'current'
-					}, {
-						data: [ { x: 0, y: enviro.ownTracksStats.statistics[1].avg} ],
-						color: '#708090',
-						name: 'average'
-				} ]
-			} );
-			SP_co2_y = new Rickshaw.Graph.Axis.Y({graph: SP_co2})
-			SP_co2_y.render();
-			SP_co2.render();
-
-			
-			/**
-			 * Third page
-             */
-			document.getElementById('TP_speed').innerHTML = "";
-			TP_speed = new Rickshaw.Graph( {
-				element: document.getElementById("TP_speed"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgSpeed} ],
-						color: '#0072ff',
-						name: 'avg_track'
-					}, {
-						data: [ { x: 0, y: enviro.sameCarStats.statistics[0].avg} ],
-						color: '#708090',
-						name: 'same_car'
-
-				} ]
-			} );
-			TP_speed_y = new Rickshaw.Graph.Axis.Y({graph: TP_speed})
-			TP_speed.render();
-			TP_speed_y.render();
-			
-			document.getElementById('TP_cons').innerHTML = "";
-			TP_cons = new Rickshaw.Graph( {
-				element: document.getElementById("TP_cons"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack:true,
-				series: [ 
-					{
-						data: [ { x: 0, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgConsumption} ],
-						color: '#0072ff'
-					}, {
-						data: [ { x: 0, y: enviro.sameCarStats.statistics[2].avg} ],
-						color: '#708090'
-
-				} ]
-			} );
-			TP_cons_y = new Rickshaw.Graph.Axis.Y({graph: TP_cons})
-			TP_cons.render();
-			TP_cons_y.render();
-
-			document.getElementById('TP_co2').innerHTML = "";
-			TP_co2 = new Rickshaw.Graph( {
-				element: document.getElementById("TP_co2"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgCo2} ],
-						color: '#0072ff'
-					}, {
-						data: [ { x: 0, y: enviro.sameCarStats.statistics[1].avg} ],
-						color: '#708090'
-
-				} ]
-			} );
-			TP_co2_y = new Rickshaw.Graph.Axis.Y({graph: TP_co2})
-			TP_co2.render();
-			TP_co2_y.render();
+		$('#SP_speed').append("<div style='position:absolute;left:" + (o.left + 2) + "px;top:" + (o.top-5) + "px;color:#FF0000; font-family: sans-serif'><b>avg " + calcLiveAverage(liveData.slice(0, ind + 1)).avgSpeed.toFixed(1) + "</b></div>");
 
 
-			/**
-			 * Last page
-             */
-			// average of all users' speed
-			document.getElementById('LP_speed').innerHTML = "";
-			LP_speed = new Rickshaw.Graph( {
-				element: document.getElementById("LP_speed"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.Speed.value}],
-						color: '#0072ff',
-						name: 'current'
-					}, {
-						data: [ { x: 0, y: enviro.stats.statistics[0].avg}],
-						color: '#708090',
-						name: 'average'
+		o = sp_speed.pointOffset({ x: -0.1, y: -2});
+		$('#SP_speed').append("<div style='position:absolute;left:" + (o.left + 2) + "px;top:" + (o.top) + "px;color: steelblue; font-family: sans-serif'><b>"+ liveData[ind].properties.phenomenons.Speed.value.toFixed(1)+" km/h</b></div>");
 
-				}
-				]
-			} );
-			LP_speed_y = new Rickshaw.Graph.Axis.Y({graph: LP_speed})
-			LP_speed.render();
-			LP_speed_y.render();
+		o = sp_speed.pointOffset({ x: -1, y: enviro.ownTracksStats.statistics[2].max + 20});
+		$('#SP_speed').append("<div style='position:absolute;left:" + (o.left + 2) + "px;top:" + (o.top-10) + "px;color: black; font-size: xx-large; font-family: sans-serif'>Speed</div>");			
+	
 
-			document.getElementById('LP_cons').innerHTML = "";
-			LP_cons = new Rickshaw.Graph( {
-				element: document.getElementById("LP_cons"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.Consumption.value}],
-						color: '#0072ff',
-						name: 'current'
-					}, {
-						data: [ { x: 0, y: enviro.stats.statistics[2].avg} ],
-						color: '#708090',
-						name: 'average'
+		var sp_cons_options = {
+			xaxis: {
+				max: 1,
+				ticks: 0
+			},
+			yaxis: {
+				max: enviro.ownTracksStats.statistics[0].max,
+				ticks: 0
+			}
+		};
+		var sp_cons = $.plot('#SP_cons', [{
+			data: [[0, liveData[ind].properties.phenomenons.Consumption.value]],
+			bars: {
+				show: true,
+				fill: 1,
+				fillColor: 'steelblue',
+				lineWidth: 0
+			},
+			color: 'steelblue',
+			shadowSize: 0
+		},{
+			data: [[0, calcLiveAverage(liveData.slice(0, ind + 1)).avgConsumption], [1, calcLiveAverage(liveData.slice(0, ind + 1)).avgConsumption]],
+			lines: {
+				show: true,
+				lineWidth: 3
+			},
+			color: '#FF0000',
+			shadowSize: 4
+		}], sp_cons_options);
 
-				} ]
-			} );
-			LP_cons_y = new Rickshaw.Graph.Axis.Y({graph: LP_cons})
-			LP_cons.render();
-			LP_cons_y.render();
+		var co = sp_cons.pointOffset({ x: 1.2, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgConsumption});
 
-			document.getElementById('LP_co2').innerHTML = "";
-			LP_co2 = new Rickshaw.Graph( {
-				element: document.getElementById("LP_co2"),
-				width: 450,
-				height: 150,
-				renderer: 'bar',
-				unstack: true,
-				series: [ 
-					{
-						data: [ { x: 0, y: liveData[ind].properties.phenomenons.CO2.value }],
-						color: '#0072ff',
-						name: 'current'
-					}, {
-						data: [ { x: 0, y: enviro.stats.statistics[1].avg} ],
-						color: '#708090',
-						name: 'average'
+		// Append it to the placeholder that Flot already uses for positioning
 
-				} ]
-			} );
-			LP_co2_y = new Rickshaw.Graph.Axis.Y({graph: LP_co2});
-			LP_co2.render();
-			LP_co2_y.render();
+		$('#SP_cons').append("<div style='position:absolute;left:" + (co.left + 2) + "px;top:" + (co.top-5) + "px;color:#FF0000; font-family: sans-serif'><b>avg " + calcLiveAverage(liveData.slice(0, ind + 1)).avgConsumption.toFixed(1) + "</b></div>");
 
-			
+
+		co = sp_cons.pointOffset({ x: -0.1, y: -0.2});
+		$('#SP_cons').append("<div style='position:absolute;left:" + (co.left + 2) + "px;top:" + (co.top) + "px;color: steelblue; font-family: sans-serif'><b>"+ liveData[ind].properties.phenomenons.Speed.value.toFixed(1)+" l/h</b></div>");
+
+		co = sp_cons.pointOffset({ x: -2.5, y: enviro.ownTracksStats.statistics[0].max + 2.5});
+		$('#SP_cons').append("<div style='position:absolute;left:" + (co.left + 2) + "px;top:" + (co.top-10) + "px;color: black; font-size: xx-large; font-family: sans-serif'>Consumption</div>");
+
+
+		var sp_co2_options = {
+			xaxis: {
+				max: 1,
+				ticks: 0
+			},
+			yaxis: {
+				max: enviro.ownTracksStats.statistics[1].max,
+				ticks: 0
+			}
+		};
+		var sp_co2 = $.plot('#SP_co2', [{
+			data: [[0, liveData[ind].properties.phenomenons.CO2.value]],
+			bars: {
+				show: true,
+				fill: 1,
+				fillColor: 'steelblue',
+				lineWidth: 0
+			},
+			color: 'steelblue',
+			shadowSize: 0
+		},{
+			data: [[0, calcLiveAverage(liveData.slice(0, ind + 1)).avgCo2], [1, calcLiveAverage(liveData.slice(0, ind + 1)).avgCo2]],
+			lines: {
+				show: true,
+				lineWidth: 3
+			},
+			color: '#FF0000',
+			shadowSize: 4
+		}], sp_co2_options);
+
+		var co2 = sp_co2.pointOffset({ x: 1.2, y: calcLiveAverage(liveData.slice(0, ind + 1)).avgCo2});
+
+		// Append it to the placeholder that Flot already uses for positioning
+
+		$('#SP_co2').append("<div style='position:absolute;left:" + (co2.left + 2) + "px;top:" + (co2.top-5) + "px;color:#FF0000; font-family: sans-serif'><b>avg " + calcLiveAverage(liveData.slice(0, ind + 1)).avgCo2.toFixed(1) + "</b></div>");
+
+
+		co2 = sp_co2.pointOffset({ x: -0.1, y: -0.5});
+		$('#SP_co2').append("<div style='position:absolute;left:" + (co2.left + 2) + "px;top:" + (co2.top) + "px;color: steelblue; font-family: sans-serif'><b>"+ liveData[ind].properties.phenomenons.CO2.value.toFixed(1)+" kg/h</b></div>");
+
+		co2 = sp_co2.pointOffset({ x: -0.6, y: enviro.ownTracksStats.statistics[1].max + 5.5});
+		$('#SP_co2').append("<div style='position:absolute;left:" + (co2.left + 2) + "px;top:" + (co2.top-10) + "px;color: black; font-size: xx-large; font-family: sans-serif'>CO2</div>");
+
+		/*###########
+		End Second slide
+		#############*/
 		}, 5000 * ind);
 	})(index);
 }
